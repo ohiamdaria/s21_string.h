2723150649effd6f5b5909b2e24d823a7769662b
gloriagi <gloriagi@student.21-school.ru>
2022-09-27 23:15:26 +0300

    trim add+

--- src/main.c
+++ src/main.c
@@ -3,11 +3,9 @@
 
 
 int main() {
-  float p;
-  int n = 0, a = 0;
-  int *q = &a;
-  char arr[] = "-243.3 11     0x12345AA    ";
-  int status = s21_sscanf(arr, " %f %d %n %p", &p, &a, &n, &q);
-  printf("status = %d\np = %f\na = %d\nn = %d\nq = %p\n", status, p, a, n, q);
+  char *one = "+!0-aeoi2o3i23iuhuhh3O*YADyagsduyoaweq213";
+  char *two = "+!0-aeoi2o3i23iuhuhh3O*YADyagsduyoaweq213";
+  // int status = s21_sscanf(arr, " %f %d %n %p", &p, &a, &n, &q);
+  printf("result: %s!\n", s21_trim(one, two));
   return 0;
 }
--- src/s21_insert.c
+++ src/s21_insert.c
@@ -1,33 +1,34 @@
 #include "s21_string.h"
 
 void *s21_insert(const char *src, const char *str, size_t start_index) {
-    char* res = s21_NULL;
-    size_t src_len = 0;
-    if (src)
-        src_len = s21_strlen(src);
-    if (src && start_index <= src_len && str) {
-        s21_size_t common_len;
-        common_len = src_len + s21_strlen(str) + 1;
-        res = (char*)malloc(sizeof(char) * common_len);
+    char *res = s21_NULL;
+
+    if (src && start_index <= s21_strlen(src) && str) {
+        s21_size_t common_len = 0;
+        common_len = s21_strlen(src) + s21_strlen(str) + 1;
+        res = (char*)calloc(common_len, sizeof(char));
         if (res) {
-            size_t i = 0;
+            s21_size_t i = 0;
             while (i < start_index) {
-                res[i] = *src;
-                i++;
+                res[i++] = *src;
                 src++;
             }
             while (*str) {
-                res[i] = *str;
-                i++;
+                res[i++] = *str;
                 str++;
             }
             while (*src) {
-                res[i] = *src;
+                res[i++] = *src;
                 src++;
-                i++;
             }
             res[i] = '\0';
         }
+    } else if (start_index == 0 && (!str)) {
+        res = s21_NULL;
+    } else if (src && start_index <= s21_strlen(src)) {
+        s21_size_t common_len = s21_strlen(src);
+        res = (char*)calloc(common_len, sizeof(char));
+        s21_strcpy(res, src);
     }
     return (void*)res;
 }
