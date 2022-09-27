cfa26d64f5062a0e137c9d2cdbe38c97fa01f605
gloriagi <gloriagi@student.21-school.ru>
2022-09-27 22:56:47 +0300

    trim add

--- src/s21_trim.c
+++ src/s21_trim.c
@@ -1,50 +1,54 @@
 #include "s21_string.h"
 
-void delete_first_letter(char *str) {
-    while (str) {
-        *str = *(str + 1);
-        str++;
+void delete_first_lex(char *str) {
+    s21_size_t count = 0;
+    while (str[count]) {
+        str[count] = str[count + 1];
+        count++;
     }
 }
 
 void rightpart(char *result, const char*src, const char *trim_chars) {
-    s21_size_t format_length = s21_strlen(trim_chars), result_length = s21_strlen(result);
-    for (int i = (int) result_length - 1; i >= 0; i--) {
-        for (s21_size_t j = 0; j < format_length; j++) {
+    s21_size_t formatlen = s21_strlen(trim_chars), reslen = s21_strlen(result);
+    int doBreak = 0, i = (int) reslen - 1;
+    while(i >= 0 && !doBreak) {
+        for (s21_size_t j = 0; j < formatlen; j++) {
             if (result[i] == trim_chars[j]) {
                 result[i] = '\0';
                 break;
             }
-            if (j == format_length - 1 && src[i] != trim_chars[format_length]) break;
+            if (j == formatlen - 1 && src[i] != trim_chars[formatlen]) doBreak = 1;
         }
+        i--;
     }
 }
 
 void leftpart(char *result, const char*src, const char *trim_chars) {
-    s21_size_t format_length = s21_strlen(trim_chars), str_length = s21_strlen(src);
-    for (s21_size_t i = 0; i < str_length; i++) {
-        for (s21_size_t j = 0; j < format_length; j++) {
+    s21_size_t formatlen = s21_strlen(trim_chars), strlen = s21_strlen(src), i = 0;
+    int doBreak = 0;
+    while(!doBreak && i < strlen) {
+        for (s21_size_t j = 0; j < formatlen; j++) {
             if (src[i] == trim_chars[j]) {
-                delete_first_letter(result);
+                delete_first_lex(result);
                 break;
             }
-            if (j == format_length - 1 && src[i] != trim_chars[format_length]) break;
+            if (j == formatlen - 1 && src[i] != trim_chars[formatlen]) doBreak = 1;
         }
+        i++;
     }
 }
-
 
 void *s21_trim(const char *src, const char *trim_chars) {
     char *result = s21_NULL;
     if (src) {
-        char default_chars[7] = " \t\n\v\r\f\0";
-        if (trim_chars == s21_NULL || s21_strlen(trim_chars) == 0) trim_chars = default_chars;
-        result = (char *) calloc(s21_strlen(src) + 10, sizeof(char));
+        if (!trim_chars || !s21_strlen(trim_chars)) trim_chars = " \t\n\v\r\f\0";
+        result = (char *) calloc(s21_strlen(src) + 13, sizeof(char));
         if (result) {
             s21_strcpy(result, (char *)src);
             leftpart(result, src, trim_chars);
             rightpart(result, src, trim_chars);
         }
     }
-    return (void *) result;
+    return (void *)result;
 }
+
