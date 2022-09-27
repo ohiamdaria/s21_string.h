3fb1b1749917e05a19813b491069cb0327d3e970
gloriagi <gloriagi@student.21-school.ru>
2022-09-27 15:26:04 +0300

    add trim

--- src/s21_trim.c
+++ src/s21_trim.c
@@ -1,35 +1,50 @@
 #include "s21_string.h"
 
+void delete_first_letter(char *str) {
+    while (str) {
+        *str = *(str + 1);
+        str++;
+    }
+}
+
+void rightpart(char *result, const char*src, const char *trim_chars) {
+    s21_size_t format_length = s21_strlen(trim_chars), result_length = s21_strlen(result);
+    for (int i = (int) result_length - 1; i >= 0; i--) {
+        for (s21_size_t j = 0; j < format_length; j++) {
+            if (result[i] == trim_chars[j]) {
+                result[i] = '\0';
+                break;
+            }
+            if (j == format_length - 1 && src[i] != trim_chars[format_length]) break;
+        }
+    }
+}
+
+void leftpart(char *result, const char*src, const char *trim_chars) {
+    s21_size_t format_length = s21_strlen(trim_chars), str_length = s21_strlen(src);
+    for (s21_size_t i = 0; i < str_length; i++) {
+        for (s21_size_t j = 0; j < format_length; j++) {
+            if (src[i] == trim_chars[j]) {
+                delete_first_letter(result);
+                break;
+            }
+            if (j == format_length - 1 && src[i] != trim_chars[format_length]) break;
+        }
+    }
+}
+
+
 void *s21_trim(const char *src, const char *trim_chars) {
-    char *buffer;
-    char *res = s21_NULL;
-    char *null_str = "";
-    char del[] = " \n\t\v\r\f";
-    if (trim_chars == s21_NULL || *trim_chars == *null_str) {
-        trim_chars = del;
-    }
+    char *result = s21_NULL;
     if (src) {
-        s21_size_t begin_trim_len;
-        begin_trim_len = s21_strspn(src, trim_chars);
-        src += begin_trim_len;
-        s21_size_t str_len = s21_strlen(src);
-        res = (char*)malloc(sizeof(char) * str_len + 1);
-        buffer = (char*)res;
-        while (*src) {
-            *res = *src;
-            res++;
-            src++;
+        char default_chars[7] = " \t\n\v\r\f\0";
+        if (trim_chars == s21_NULL || s21_strlen(trim_chars) == 0) trim_chars = default_chars;
+        result = (char *) calloc(s21_strlen(src) + 10, sizeof(char));
+        if (result) {
+            s21_strcpy(result, (char *)src);
+            leftpart(result, src, trim_chars);
+            rightpart(result, src, trim_chars);
         }
-        *res = '\0';
-        res--;
-        int ch = *res;
-        while (s21_strchr(trim_chars, ch) && str_len > 0) {
-            *res = '\0';
-            res--;
-            ch = (int)*res;
-            str_len--;
-        }
-        res = buffer;
     }
-    return (void*)res;
+    return (void *) result;
 }
